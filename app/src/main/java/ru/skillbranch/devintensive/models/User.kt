9f024f34557debbcb31850eca01967d6b5ab3de5
Don Luis Nazario De Lima <knizhkilitres@inbox.ru>
2019-07-07 16:05:52 +0300

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -19,8 +19,13 @@ data class User (
         id = id,
         firstName = firstName,
         lastName = lastName,
-        avatar = null
+        avatar = null,
+        rating = 0,
+        respect = 0,
+        lastVisit = Date(),
+        isOnline = false
     )
+
     constructor(id:String) : this(id, "John", "Doe")
 
     init {
@@ -58,14 +63,24 @@ data class User (
         }
     }
 
-    fun build(): User{
-        return User(id = id,
-                    firstName = firstName,
-                    lastName = lastName,
-                    avatar = avatar,
-                    rating = rating,
-                    respect = respect,
-                    lastVisit = lastVisit,
-                    isOnline = isOnline)
+    data class Builder(
+        var id: String = "",
+        var firstName: String? = null,
+        var lastName: String? = null,
+        var avatar: String? = null,
+        var rating: Int = 0,
+        var respect: Int = 0,
+        var lastVisit: Date? = null,
+        var isOnline: Boolean = false) {
+
+        fun id(id: String) = apply {this.id = id}
+        fun firstName(firstName: String) = apply {this.firstName = firstName}
+        fun lastName(lastName: String) = apply {this.lastName = lastName}
+        fun avatar(avatar: String) = apply {this.avatar = avatar}
+        fun rating(rating: Int) = apply {this.rating = rating}
+        fun respect(respect: Int) = apply {this.respect = respect}
+        fun lastVisit(lastVisit: Date) = apply {this.lastVisit = lastVisit}
+        fun isOnline(isOnline: Boolean) = apply {this.isOnline = isOnline}
+        fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
     }
 }
