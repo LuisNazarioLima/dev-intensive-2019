c4e84c73deb82e9d3a786da4dc3a746213be9d00
Don Luis Nazario De Lima <knizhkilitres@inbox.ru>
2019-07-14 23:56:15 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -13,6 +13,9 @@ var wrongAnswer: Short = 0
     }
 
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
+        if (!question.validation(answer).second) {
+            return "${question.validation(answer).first}\n${question.question}" to status.color
+        }
         return if(question.answers.contains(answer)) {
                 question = question.nextQuestion()
                 "Отлично - ты справился\n${question.question}" to status.color
@@ -48,23 +51,96 @@ var wrongAnswer: Short = 0
     enum class Question(val question: String, val answers: List<String>){
         NAME("Как меня зовут?", listOf("Бендер", "Bender")) {
             override fun nextQuestion(): Question = PROFESSION
+
+            override fun validation(input: String): Pair<String, Boolean> {
+                return if (input.substring(0,1) != input.substring(0,1).toUpperCase()) {
+                    "Имя должно начинаться с заглавной буквы" to false
+                } else {
+                    "" to true
+                }
+            }
         },
         PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")){
             override fun nextQuestion(): Question = MATERIAL
+            override fun validation(input: String): Pair<String, Boolean> {
+                return if (input.substring(0,1) != input.substring(0,1).toLowerCase()) {
+                    "Профессия должна начинаться со строчной буквы" to false
+                } else {
+                    "" to true
+                }
+            }
         },
         MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")){
             override fun nextQuestion(): Question = BDAY
+            override fun validation(input: String): Pair<String, Boolean> {
+                var errorExist: Boolean = false
+                for (char in input) {
+                    errorExist = when (char){
+                        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> true
+                        else -> false
+                    }
+                    if (errorExist) {
+                        break
+                    }
+                }
+                return if (errorExist) {
+                    "Материал не должен содержать цифр" to false
+                } else {
+                    "" to true
+                }
+            }
         },
         BDAY("Когда меня создали?", listOf("2993")){
             override fun nextQuestion(): Question = SERIAL
+            override fun validation(input: String): Pair<String, Boolean> {
+                var errorExist: Boolean = false
+                for (char in input) {
+                    errorExist = when(char) {
+                        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> false
+                        else -> true
+                    }
+                    if (errorExist) {
+                        break
+                    }
+                }
+                return if (errorExist) {
+                    "Год моего рождения должен содержать только цифры" to false
+                } else {
+                    "" to true
+                }
+            }
         },
         SERIAL("Мой серийный номер?", listOf("2716057")){
             override fun nextQuestion(): Question = IDLE
+            override fun validation(input: String): Pair<String, Boolean> {
+                var errorExist: Boolean = false
+                if (input.length != 7) {
+                    return "Серийный номер содержит только цифры, и их 7" to false
+                }
+                for (char in input) {
+                    errorExist = when(char) {
+                        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> false
+                        else -> true
+                    }
+                    if (errorExist) {
+                        break
+                    }
+                }
+                return if (errorExist) {
+                    "Серийный номер содержит только цифры, и их 7" to false
+                } else {
+                    "" to true
+                }
+            }
         },
         IDLE("На этом все, вопросов больше нет", listOf()){
             override fun nextQuestion(): Question = IDLE
+            override fun validation(input: String): Pair<String, Boolean> {
+                return "" to true
+            }
         };
 
         abstract fun nextQuestion(): Question
+        abstract fun validation(input: String): Pair<String, Boolean>
     }
 }
