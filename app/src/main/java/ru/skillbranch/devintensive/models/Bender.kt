a2f6c7dfcaeb98746a20a92e6f02424c901407bf
Don Luis Nazario De Lima <knizhkilitres@inbox.ru>
2019-07-14 20:20:01 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -15,7 +15,7 @@ var wrongAnswer: Short = 0
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
         return if(question.answers.contains(answer)) {
                 question = question.nextQuestion()
-                "Отлично - это правильный ответ!\n${question.question}" to status.color
+                "Отлично - ты справился\n${question.question}" to status.color
             } else {
                 wrongAnswer++
                 if (wrongAnswer == 3.toShort()) {
@@ -25,7 +25,7 @@ var wrongAnswer: Short = 0
                     "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
                 } else {
                     status = status.nextStatus()
-                    "Это неправильный ответ!\n${question.question}" to status.color
+                    "Это неправильный ответ\n${question.question}" to status.color
                 }
             }
         }
