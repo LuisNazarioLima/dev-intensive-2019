faf6c3ff765546531ecf03e5da7b31dfb4b3ab3a
Don Luis Nazario De Lima <knizhkilitres@inbox.ru>
2019-07-16 22:27:26 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -6,10 +6,11 @@ import android.content.Context.INPUT_METHOD_SERVICE
 import android.view.inputmethod.InputMethodManager
 import androidx.core.content.ContextCompat.getSystemService
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.R
 
 
 fun Activity.hideKeyboard() {
-    et_message.clearFocus()
     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(et_message.getWindowToken(), 0)
+    //imm.hideSoftInputFromWindow(et_message.getWindowToken(), 0)
+    imm.hideSoftInputFromWindow(et_message.windowToken, 0);
 }
