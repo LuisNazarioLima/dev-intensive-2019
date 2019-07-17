126804e1b771ea4517e83da75dc8c3e94b252c51
Don Luis Nazario De Lima <knizhkilitres@inbox.ru>
2019-07-17 22:38:45 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -3,6 +3,8 @@ package ru.skillbranch.devintensive.extensions
 import android.app.Activity
 import android.content.Context
 import android.content.Context.INPUT_METHOD_SERVICE
+import android.graphics.Rect
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.core.content.ContextCompat.getSystemService
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,4 +15,24 @@ fun Activity.hideKeyboard() {
     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     //imm.hideSoftInputFromWindow(et_message.getWindowToken(), 0)
     imm.hideSoftInputFromWindow(et_message.windowToken, 0);
+}
+
+fun Activity.isKeyboardOpen(rootView: View): Boolean {
+    val visibleBounds = Rect()
+
+    rootView.getRootView().getWindowVisibleDisplayFrame(visibleBounds)
+    val dm = rootView.resources.displayMetrics
+    val heightDiff = rootView.bottom - visibleBounds.bottom
+
+    return heightDiff > 100 * dm.density
+}
+
+fun Activity.isKeyboardClosed(rootView: View): Boolean {
+    val visibleBounds = Rect()
+
+    rootView.getRootView().getWindowVisibleDisplayFrame(visibleBounds)
+    val dm = rootView.resources.displayMetrics
+    val heightDiff = rootView.bottom - visibleBounds.bottom
+
+    return heightDiff < 100 * dm.density
 }
